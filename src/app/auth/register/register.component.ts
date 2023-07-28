9cc9047470407b7abd5085fe2d0876ae35fbfb2e
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-28 15:54:20 -0500

    [ADD] register styles

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm:FormGroup = new FormGroup({
+    name : new FormControl('', Validators.required),
+    last_name : new FormControl('', Validators.required),
+    email : new FormControl('', Validators.required),
+    prefix : new FormControl('', Validators.required),
+    cellphone : new FormControl('', Validators.required),
+    password : new FormControl('', Validators.required),
+    met : new FormControl('', Validators.required),
+    referral_code : new FormControl('', Validators.required),
+  })
+
+  onSubmit(){
+    
+  }
 }
