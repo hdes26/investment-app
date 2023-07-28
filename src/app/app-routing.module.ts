b1a14959c2f6b9d419074f4e0d3cbb166c340312
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-28 12:03:16 -0500

    config project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './auth/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'register', component:RegisterComponent},
+  {path:'**',redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
