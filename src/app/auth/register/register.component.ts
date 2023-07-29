82eea9b2401369a2a34b543260f0a4b03ce9e118
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-29 08:44:51 -0500

    [UPDATE] register feature finish :sparkles:

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,25 +1,44 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    last_name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    prefix: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    cellphone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    met: new FormControl('', [Validators.required, this.validateDropdown]),
+    referral_code: new FormControl('', [Validators.pattern('^[0-9]+$')]),
+    terms_condition: new FormControl(false, [Validators.requiredTrue, Validators.required]),
+  });
+  showPassword: boolean = false;
+  metOptions: string[] = ['Linkedin', 'Torre', 'Glassdoor', 'Otros'];
 
-  registerForm:FormGroup = new FormGroup({
-    name : new FormControl('', Validators.required),
-    last_name : new FormControl('', Validators.required),
-    email : new FormControl('', Validators.required),
-    prefix : new FormControl('', Validators.required),
-    cellphone : new FormControl('', Validators.required),
-    password : new FormControl('', Validators.required),
-    met : new FormControl('', Validators.required),
-    referral_code : new FormControl('', Validators.required),
-  })
 
-  onSubmit(){
-    
+  validateDropdown(control: AbstractControl) {
+    if (!control.value) {
+      return { required: true };
+    }
+    return null;
+  }
+  onCheckboxChange() {
+    const checkboxValue = this.registerForm.get('terms_condition')?.value;
+    if (checkboxValue) {
+      this.registerForm.get('terms_condition')?.setValue(false);
+    } else {
+      this.registerForm.get('terms_condition')?.setValue(true);
+    }
+  }
+  togglePasswordVisibility(){
+    this.showPassword = !this.showPassword;
+
+  }
+  onSubmit() {
   }
 }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+    name:string;
+    last_name:string;
+    email:string;
+    prefix:number;
+    cellphone:number;
+    password:string;
+    met:string;
+    referral_code:number;
+}
