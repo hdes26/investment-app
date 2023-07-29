3f62d4724fc9bcdf80a51d618a51b64f3398140e
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-29 10:06:42 -0500

    [DELETE] useless spaces

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -1,9 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-;
-
-
 
 export const authRoutes:Routes = [
     {path:'login', component:LoginComponent},
