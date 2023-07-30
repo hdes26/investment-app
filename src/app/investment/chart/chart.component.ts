880518c93a73ee64eb177e47df0df4d6dda9ae1c
HernanLya <130484267+HernanLya@users.noreply.github.com>
2023-07-30 10:44:02 -0500

    [ADD] funcionality v2 :sparkles:

--- src/app/investment/chart/chart.component.ts
+++ src/app/investment/chart/chart.component.ts
@@ -18,20 +18,20 @@ export class ChartComponent implements OnInit {
           data: [12, 19, 3, 5, 2, 3],
           backgroundColor: "#0196FD",
           borderColor: "#0196FD",
-          borderWidth: 1
+          borderWidth: 1,
+          borderRadius: 50
         },
-        {
-          label: 'Dat21',
-          data: [19, 12, 5, 3, 1, 6],
-          backgroundColor: "#FFAF00",
-          borderColor: "#FFAF00",
-          borderWidth: 1
-        }]
+        ]
       },
       options: {
         scales: {
           y: {
             beginAtZero: true
+          }
+        },
+        plugins: {
+          legend: {
+            display: false
           }
         }
       }
--- src/app/investment/investment.component.ts
+++ src/app/investment/investment.component.ts
@@ -9,13 +9,36 @@ import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/fo
 export class InvestmentComponent {
   investmentForm: FormGroup = new FormGroup({
     project: new FormControl('', [Validators.required, this.validateDropdown]),
-    amount: new FormControl('', [Validators.required, Validators.minLength(8),Validators.pattern('^[0-9]+$')]),
-    units: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    amount: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    units: new FormControl(0, [Validators.required, Validators.pattern('^[0-9]+$')]),
     terms_condition: new FormControl(false, [Validators.requiredTrue]),
   });
   projectOptions: string[] = ['Nido de agua ', 'Indie Universe'];
   buttonStyle = 'cursor: not-allowed;';
 
+
+  onInputChange() {
+    if (isNaN(parseInt(this.investmentForm.get('amount')?.value))) {
+      this.investmentForm.get('amount')?.setValue('');
+      return;
+    }
+
+    const value: number = parseInt(this.investmentForm.get('amount')?.value.replace(/\./g, ''), 10)
+    if (value >= 10000000) {
+      this.investmentForm.get('units')?.setValue(this.calculateUnits(value));
+    }else{
+      this.investmentForm.get('units')?.setValue(0);
+    }
+
+    const formatValue = value.toLocaleString('es');
+
+    this.investmentForm.get('amount')?.setValue(formatValue.toString());
+
+  }
+  calculateUnits(value: number): number {
+    const unitValue = 100000;  
+    return Math.trunc(value/unitValue);
+  }
   onCheckboxChange() {
     const checkboxValue = this.investmentForm.get('terms_condition')?.value;
     if (checkboxValue) {
@@ -24,16 +47,14 @@ export class InvestmentComponent {
       this.investmentForm.get('terms_condition')?.setValue(true);
     }
   }
-
   validateDropdown(control: AbstractControl) {
     if (!control.value) {
       return { required: true };
     }
     return null;
   }
-
   onSubmit() {
-    console.log('hola');
-    
+    const number: number = 11000000;
+    console.log(number.toLocaleString('es-CO'));
   }
 }
